4a5014c7b0a0c891ea781d8290f790de14ac6886
CarlosDR <dimatteoca@gmail.com>
2024-05-07 10:46:35 -0400

    change final message

--- src/components/game-over/GameOver.tsx
+++ src/components/game-over/GameOver.tsx
@@ -1,14 +1,15 @@
-
 import PokeBtn from './PokeBtn';
 import './game-over.css';
 
-export type GameOverPropType=
-{setGameover:React.Dispatch<React.SetStateAction<boolean>>}
-const GameOver = ({ setGameover }:GameOverPropType) => {
+export type GameOverPropType = {
+  setGameover: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const GameOver = ({ setGameover }: GameOverPropType) => {
+  
   return (
     <>
       <div className='msg__container__gameOver'>
-        <p className='gameOver__message'>Game Over!</p>
+        <p className='gameOver__message'>You Win!</p>
       </div>
       <PokeBtn setGameover={setGameover} />
     </>
